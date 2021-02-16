2342d6c46b6316b66d6175b27b67d649c1bdc573
Saumya <saumy.sh@gmail.com>
2021-02-16 09:29:22 +0530

    First Commit

---
+++ HelloWorld.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+   int main(){
+
+
+    int num;
+    printf("Enter an integer\n");
+    scanf("%d",&num);
+
+    num= num+1;
+   printf("Output is %d\n",num);
+   return 0;
+}
